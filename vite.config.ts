1732f3e0a78b0af839d5744533e5fececaf70819
Edgar <goodfreelance0@gmail.com>
2025-12-09 03:18:16 +0200

    init

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@client': path.resolve(__dirname, 'src/client'),
+      '@server': path.resolve(__dirname, 'src/server'),
+      '@shared': path.resolve(__dirname, 'src/shared')
+    }
+  }
+});
